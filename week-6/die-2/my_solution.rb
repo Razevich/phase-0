a6dd77a4f9dca6ff180e46a12992bc92d0ea0b50
Razevich <razevich.c@gmail.com>
2015-09-21 16:23:31 -0700

    first solution

---
+++ week-6/die-2/my_solution.rb
@@ -0,0 +1,51 @@
+# Die Class 2: Arbitrary Symbols
+
+
+# I worked on this challenge [by myself, with: ].
+# I spent [#] hours on this challenge.
+
+# Pseudocode Give an array of strings and convert them into a dice. The length of string = the size of the dice. Return a random part of the original orray as output
+
+# Input: Array
+# Output: Random indivudal string
+# Steps: 1 Create argument error
+          # 2 set label to instance variable
+          # 3 set sides to label length
+          # 4 set roll to random index of label array
+
+
+# Initial Solution
+
+class Die
+  def initialize(labels)
+    @labels = labels
+    if labels == []
+      raise ArgumentError.new("X boop beep")
+    end
+  end
+
+  def sides
+    @sides = @labels.length
+  end
+
+  def roll
+    roll = @labels.sample
+  end
+end
+
+die = Die.new(["a", "b","d"])
+p die.roll
+# Refactored Solution
+
+
+
+
+
+
+
+
+# Reflection
+# The difference is the use of strings instead of numbers. I had to change my solution slightly.
+#It shows me that making simple code that is simple and easy to transfer to different code so that I can re use basics.
+#I learned about .sample which takes a random spot in an array.
+#Instance and local variables were cleared up for me.
